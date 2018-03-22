f51c653966bb7f351d2a19b8615d67144a0e0f39
Rob Phoenix <rob@robphoenix.com>
2018-03-22 20:46:15 +0000

    Connect store to store picker

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react"
+import { getFunName } from "../helpers"
 
 class StorePicker extends Component {
+  storeInput = React.createRef()
+  goToStore = e => {
+    e.preventDefault()
+    const storeName = this.storeInput.value.value
+    this.props.history.push(`/store/${storeName}`)
+  }
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please enter a store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input type="text" ref={this.storeInput} required placeholder="Store Name" defaultValue={getFunName()} />
         <button type="submit">Visit Store</button>
       </form>
     )
