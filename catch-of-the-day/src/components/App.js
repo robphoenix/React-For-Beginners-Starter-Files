349b1b1d61bf259c575ad6b469c64808a91e355e
Rob Phoenix <rob@robphoenix.com>
2018-03-23 17:19:30 +0000

    Load sample fishes

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,17 +2,24 @@ import React from "react"
 import Header from "./Header"
 import Order from "./Order"
 import Inventory from "./Inventory"
+import sampleFishes from "../sample-fishes"
 
 export class App extends React.Component {
   state = {
     fishes: {},
     order: {}
   }
+
   addFish = fish => {
     const fishes = { ...this.state.fishes }
     fishes[`fish${Date.now()}`] = fish
     this.setState({ fishes })
   }
+
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes })
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -20,7 +27,10 @@ export class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
       </div>
     )
   }
