1a8934b20263b683e1d300087718f2adb66421f9
Rob Phoenix <rob@robphoenix.com>
2018-03-27 17:29:00 +0100

    Update state in form

---
+++ catch-of-the-day/src/components/EditFishForm.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react"
+
+export class EditFishForm extends Component {
+  handleChange = e => {
+    const updatedFish = {
+      ...this.props.fish,
+      [e.currentTarget.name]: e.currentTarget.value
+    }
+    this.props.updateFish(this.props.index, updatedFish)
+  }
+  render() {
+    return (
+      <div className="fish-edit">
+        <input
+          type="text"
+          name="name"
+          onChange={this.handleChange}
+          value={this.props.fish.name}
+        />
+        <input
+          type="text"
+          name="price"
+          onChange={this.handleChange}
+          value={this.props.fish.price}
+        />
+        <select name="status">
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold Out!</option>
+        </select>
+        <textarea
+          name="desc"
+          onChange={this.handleChange}
+          value={this.props.fish.desc}
+        />
+        <input
+          type="text"
+          name="image"
+          onChange={this.handleChange}
+          value={this.props.fish.image}
+        />
+      </div>
+    )
+  }
+}
+
+export default EditFishForm
